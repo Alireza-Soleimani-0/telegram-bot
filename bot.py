7d23cd46bc950a79ab7c63866409cb27b6049c2c
Alireza-Soleimani <165762170+Alireza-Soleimani-0@users.noreply.github.com>
2026-02-18 08:03:47 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,50 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = "8404253345:AAGQvz4QvmUjF7I4RDZA6j28fXQuzSYj-UA"
+
+keyboard = [
+    ["🔗 LinkedIn"],
+    ["💻 Stack Overflow"],
+    ["🐙 GitHub"],
+    ["🛡 ASnet Security"],
+    ["📩 𝗔.𝗦 Anonymous"]
+]
+
+reply_markup = ReplyKeyboardMarkup(
+    keyboard,
+    resize_keyboard=True
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🔥 Welcome to 𝗔𝗹𝗶𝗿𝗲𝘇𝗮 𝗦𝗼𝗹𝗲𝗶𝗺𝗮𝗻𝗶 Bot\n\nSelect an option:",
+        reply_markup=reply_markup
+    )
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "🔗 LinkedIn":
+        await update.message.reply_text("https://www.linkedin.com/in/alirezasoleimani-")
+
+    elif text == "💻 Stack Overflow":
+        await update.message.reply_text("https://stackoverflow.com/users/23951445/alireza")
+
+    elif text == "🐙 GitHub":
+        await update.message.reply_text("https://github.com/Alireza-Soleimani-0")
+
+    elif text == "🛡 ASnet Security":
+        await update.message.reply_text("https://t.me/ASnet01")
+
+    elif text == "📩 𝗔.𝗦 Anonymous":
+        await update.message.reply_text("https://t.me/NoronChat_bot?start=sec-fhhchicadf")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+print("Bot is running...")
+app.run_polling()
